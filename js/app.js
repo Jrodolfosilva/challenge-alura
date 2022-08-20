a2b10bcccc6903a6b8e54ee0fe684dd53701d43e
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-08-20 00:26:55 -0300

    Up files

---
+++ js/app.js
@@ -0,0 +1,69 @@
+const chave = {
+    A:"p",
+    B:"o",
+    C:"i",
+    D:"u",
+    E:"y",
+    F:"t",
+    G:"r",
+    H:"e",
+    I:"w",
+    J:"q",
+    K:"a",
+    L:"s",
+    M:"d",
+    N:"f",
+    O:"g",
+    P:"h",
+    Q:"j",
+    R:"k",
+    S:"l",
+    T:"ç",
+    U:"z",
+    V:"x",
+    W:"c",
+    X:"v",
+    Y:"b",
+    Z:"n"
+
+}
+
+const tex = "JOSE" //toUperaser
+let texto = tex.split("")
+let pa = []
+for(let i=0; i < texto.length; i++ ){
+  pa.push(chave[texto[i]])
+
+}
+
+console.log(pa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
